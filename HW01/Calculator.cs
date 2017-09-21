0f1019b62198d0159785197ce785d20a7dd2c5ad
Alo Pullmann <alpull@ttu.ee>
2017-09-21 21:11:35 +0300

    HW01, initial commit

---
+++ HW01/Calculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace HW01 {
+    class Calculator {
+        static void Main(string[] args) {
+            double a, b, sum, subtract, multiply, divide;
+
+            Console.WriteLine("See on nüüd selline imelik kalkulaator, mis oskab kahe arvuga igasuguseid vigureid teha:" +
+                              "\nsummat, vahet, korrutist ja jagatist arvutada ning lõpuks summast ruutjuurt võtta.");
+            Console.Write("\nSisesta esimene arv: ");
+            a = Double.Parse(Console.ReadLine());
+
+            Console.Write("Sisesta teine arv: ");
+            b = Double.Parse(Console.ReadLine());
+
+            sum = a + b;
+            Console.WriteLine("\nKahe arvu summa on: " + sum + 
+                              "\nVajuta 'Enter', et kahe arvu vahe leida.");
+            Console.ReadLine();
+
+            subtract = a - b;
+            Console.WriteLine("Kahe arvu vahe on: " + subtract +
+                              "\nVajuta 'Enter', et kahe arvu korrutis leida.");
+            Console.ReadLine();
+
+            multiply = a * b;
+            Console.WriteLine("Kahe arvu korrutis on: " + multiply +
+                              "\nVajuta 'Enter', et kahe arvu jagatis leida.");
+            Console.ReadLine();
+
+            divide = a / b;
+            Console.WriteLine("Kahe arvu jagatis on: " + divide + 
+                              "\nVajuta 'Enter', et kahe arvu summast ruutjuur võtta.");
+            Console.ReadLine();
+            
+            double sqrt = Math.Sqrt(sum);
+            if (sum < 0) {
+                Console.WriteLine("Negatiivsest arvust ei saa ruutjuurt võtta!");
+            } else {
+                Console.WriteLine("Ruutjuur summast on: " + sqrt);
+            }
+
+            Console.WriteLine("\nVajuta 'Enter', et rakendusest väljuda.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HW01/Questionnaire.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace HW01 {
+    class Questionnaire {
+        static void Main(string[] args) {
+            int correctAnswers = 0;
+
+            Console.Write("Tere tulemast Onu Alo küsimustikku vastama!" +
+                          "\n1. Milline neist vulkaanidest purskas 2010. aastal? Vesuuv, Yellowstone või Eyjafjallajökull?" +
+                          "\nVastus: ");
+            string firstAnswer = Console.ReadLine();
+            string firstCorrect = "Eyjafjallajökull";
+
+            if (firstAnswer == firstCorrect) {
+                correctAnswers++;
+            }
+
+            Console.Write("2. Mitu stuudioalbumit on Led Zeppelin oma karjääri jooksul väljastanud? 9, 11 või 13?" +
+                          "\nVastus: ");
+            int secondAnswer = Int32.Parse(Console.ReadLine());
+            int secondCorrect = 9;
+
+            if (secondAnswer == secondCorrect) {
+                correctAnswers++;
+            }
+
+            Console.Write("3. Mis on Euleri arvu ligikaudne väärtus? 2.718, 3.142 või 1.337?" +
+                          "\nVastus: ");
+            double thirdAnswer = Double.Parse(Console.ReadLine());
+            double thirdCorrect = 2.718;
+
+            if (thirdAnswer == thirdCorrect) {
+                correctAnswers++;
+            }
+
+            Console.WriteLine("\nOled õigesti vastanud " + correctAnswers + " küsimust.");
+            if (correctAnswers == 3) {
+                Console.WriteLine("Hästi tehtud! Kõik vastused on õiged!");
+            } else {
+                Console.WriteLine("Kahjuks ei olnud kõik vastused õiged. Proovi uuesti!");
+            }
+
+            Console.WriteLine("\nVajuta 'Enter', et väljuda rakendusest.");
+            Console.ReadLine();
+        }
+    }
+}
