9e11fdd6a34369ef1fec27fa3372978960aaa797
Alo Pullmann <alpull@ttu.ee>
2017-09-21 21:19:09 +0300

    HW03, initial commit

---
+++ HW03/BankCard.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace HW03 {
+    public class BankCard {
+        private double _balance;
+        private string _cardType;
+        private int _cardNumber;
+
+        public BankCard() {
+            _balance = 0;
+            _cardType = "Visa";
+        }
+
+        public BankCard(int balance, string cardType) {
+            _balance = balance;
+            _cardType = cardType;
+        }
+
+        public void SetCardNumber(string cardNumber) {
+            if (cardNumber.Length != 8) {
+                Console.WriteLine("Invalid value, the correct format is 8 digits!\n");
+            } else {
+                _cardNumber = int.Parse(cardNumber);
+                Console.WriteLine("The card number is: {0}\n", _cardNumber);
+            }
+        }
+
+        public void PrintCardType() {
+            Console.WriteLine("The card type is: {0}\n", _cardType);
+        }
+
+        public void PrintBalance() {
+            Console.WriteLine("Your balance is: {0}\n", _balance);
+        }
+
+        public void IncreaseBalance(double deposit) {
+            _balance += deposit;
+            Console.WriteLine("You have successfully deposited {0}" +
+                              "\nYour balance is: {1}\n", deposit, _balance);
+        }
+
+        public void DecreaseBalance(double withdraw) {
+            double endBalance = _balance -= withdraw;
+
+            if (endBalance < 0) {
+                Console.WriteLine("Cannot do this operation, insufficient funds!\n");
+            } else {
+                _balance = endBalance;
+                Console.WriteLine("You have successully withdrawn {0}" +
+                                  "\nYour balance is: {1}\n", withdraw, _balance);
+            }
+        }
+    }
+}
---
+++ HW03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using HW03;
+
+namespace EX03 {
+    public class Program {
+        public static void Main(string[] args) {
+            BankCard bankCard1 = new BankCard();
+            BankCard bankCard2 = new BankCard(200, "Maestro");
+            
+            bankCard1.SetCardNumber("12345678");
+            bankCard2.SetCardNumber("42");
+            
+            bankCard1.IncreaseBalance(75.52);
+            bankCard1.PrintBalance();
+            
+            bankCard1.DecreaseBalance(54.99);
+            bankCard2.DecreaseBalance(252);
+
+            Console.ReadLine();
+        }
+    }
+}
