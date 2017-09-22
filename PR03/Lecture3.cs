2e936c93a66e01767c5c16062846fa74a6b1df71
Alo Pullmann <alpull@ttu.ee>
2017-09-22 10:32:55 +0300

    PR03, added Lecture3.cs

---
+++ PR03/Lecture3.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+
+namespace PR03 {
+    public class Lecture3 {
+        static void Lecture() {
+            int a = 5;
+            bool notTrue = false;
+
+            string b = WhatEven("Juhan", a).ToString();
+            string c = ReverseString(b);
+        }
+
+        static int ReturnSumOfTwoInts(int a, int b) {
+            return a + b;
+        }
+
+        static void Method(string a, string b) {
+        }
+
+        static bool WhatEven(string a, int b) {
+            return true;
+        }
+
+        static double SumOfFourDoubles(double a, double b, double c, double d) {
+            return a + b + c + d;
+        }
+
+        static string ReverseString(string a) {
+            string b = a.Reverse().ToString();
+            
+            return b;
+        }
+    }
+}
