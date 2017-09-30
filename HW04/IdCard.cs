0568dac8793a324436ede12439950de89caf2389
Alo Pullmann <alpull@ttu.ee>
2017-09-30 20:02:45 +0300

    HW04, a lot of changes

--- HW04/IdCard.cs
+++ HW04/IdCard.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using Microsoft.SqlServer.Server;
 
 namespace HW04 {
     public class IdCard {
-        private int _sex;
-        private DateTime _date;
-        private int _birthPlace;
+        private string _id;
+        private string _sex;
+        private string _date;
+        private int _birthCode;
+        private string _birthArea;
         private int _controlNumber;
         
         public IdCard() {
@@ -16,15 +16,15 @@ namespace HW04 {
         }
 
         public void DecodeId(string id) {
-            string sex;
+            _id = id;
+
+            int sex = Int32.Parse(id.Substring(0, 1));
+            _sex = sex % 2 == 0 ? "naine" : "mees";
+
             string birthYearFirstHalf;
-
-            _sex = Int32.Parse(id.Substring(0, 1));
-            sex = _sex % 2 == 0 ? "naine" : "mees";
-
-            if (_sex == 1 || _sex == 2) {
+            if (sex == 1 || sex == 2) {
                 birthYearFirstHalf = "18";
-            } else if (_sex == 3 || _sex == 4) {
+            } else if (sex == 3 || sex == 4) {
                 birthYearFirstHalf = "19";
             } else {
                 birthYearFirstHalf = "20";
@@ -34,16 +34,26 @@ namespace HW04 {
             string birthYear = birthYearFirstHalf + birthYearSecondHalf;
             string birthMonth = id.Substring(3, 2);
             string birthDay = id.Substring(5, 2);
-            _date = new DateTime(Int32.Parse(birthYear), Int32.Parse(birthMonth), Int32.Parse(birthDay));
-            string date = _date.ToShortDateString();
             
-            _birthPlace = Int32.Parse(id.Substring(7, 3));
+            DateTime date = new DateTime(Int32.Parse(birthYear), 
+                Int32.Parse(birthMonth), Int32.Parse(birthDay));
+            _date = date.ToShortDateString();
+            
+            _birthCode = Int32.Parse(id.Substring(7, 3));
             _controlNumber = Int32.Parse(id.Substring(10, 1));
             
             Console.WriteLine("Isik isikukoodiga {0} on sündinud {1}. Ta on {2}, " +
                               "kelle isikukood registreeriti {3} haiglas ja sel päeval oli ta {4} {5}. " +
                               "Isikukoodi kontrollnumbriks on {6}.",
-                id, date, sex, _birthPlace, _controlNumber, _controlNumber, _controlNumber);
+                _id, _date, _sex, _birthCode, _controlNumber, _controlNumber, _controlNumber);
+        }
+
+        public void GetBirthPlace() {
+            switch (_birthCode) {
+                    case int i when (i >= 1 && i <= 10):
+                        _birthArea = "Kuressaare";
+                        break;
+            }
         }
     }
 }
