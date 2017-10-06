71ba2df5185b11e6f32c3825d78d00f1e48514a4
Alo Pullmann <alpull@ttu.ee>
2017-10-06 21:24:46 +0300

    HW05: initial commit

---
+++ HW05/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW05 {
+    internal class Program {
+        public static void Main(string[] args) {
+            
+        }
+
+        public static int GetCompoundInterest(int initialSum, int interestRate, int years) {
+            for () {
+                    
+                }
+        }
+    }
+}
