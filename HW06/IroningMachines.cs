6d0e11ff01740498c82b3ecc0843afff44000bf7
Alo Pullmann <alpull@ttu.ee>
2017-10-13 16:57:28 +0300

    HW06: wrote additional if-statements

--- HW06/IroningMachines.cs
+++ HW06/IroningMachines.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 
 namespace HW06 {
-    // TODO: check all methods with whether or not the machine is on
-    
     interface IIroningMachines {
         void Descale();
         void DoIroning(int temperature);
@@ -24,55 +22,71 @@ namespace HW06 {
         internal string _output = "{0} is ironing with {1} degrees";
 
         public void Descale() {
-            _ironingCounter = 0;
-            Console.WriteLine("Machine is clean.");
+            if (!_isPowerOn) {
+                Console.WriteLine("Turn the machine on first!");
+            } else {
+                _ironingCounter = 0;
+                Console.WriteLine("Machine is clean.");
+            }
         }
 
         public virtual void DoIroning(int temperature) {
-            _temperature = temperature;
-            if (_ironingCounter != 3) {
-                if (_temperature >= MINIMUM_TEMP && _temperature <= SYNTHETICS_MAX) {
-                    Console.WriteLine(_machineType + " is ironing with Synthetics program.");
-                } else if (_temperature > SYNTHETICS_MAX && _temperature <= SILK_MAX) {
-                    Console.WriteLine(_machineType + " is ironing with Silk program.");
-                } else if (_temperature > SILK_MAX && _temperature <= COTTON_MAX) {
-                    Console.WriteLine(_machineType + " is ironing with Cotton program.");
+            if (!_isPowerOn) {
+                Console.WriteLine("Turn the machine on first!");
+            } else {
+                _temperature = temperature;
+                if (_ironingCounter != 3) {
+                    if (_temperature >= MINIMUM_TEMP && _temperature <= SYNTHETICS_MAX) {
+                        Console.WriteLine(_machineType + " is ironing with Synthetics program.");
+                    } else if (_temperature > SYNTHETICS_MAX && _temperature <= SILK_MAX) {
+                        Console.WriteLine(_machineType + " is ironing with Silk program.");
+                    } else if (_temperature > SILK_MAX && _temperature <= COTTON_MAX) {
+                        Console.WriteLine(_machineType + " is ironing with Cotton program.");
+                    } else
+                        Console.WriteLine("Invalid temperature range for ironing!");
+                    _ironingCounter++;
                 } else
-                    Console.WriteLine("Invalid temperature range for ironing!");
-                _ironingCounter++;
-            } else
-                Console.WriteLine("The machine has been used 3 times and needs cleaning.");
+                    Console.WriteLine("The machine has been used 3 times and needs cleaning.");
+            }
         }
 
         public virtual void DoIroning(string program) {
-            string output = string.Format(_output, _machineType, _temperature);
+            if (!_isPowerOn) {
+                Console.WriteLine("Turn the machine on first!");
+            } else {
+                string output = string.Format(_output, _machineType, _temperature);
 
-            if (_ironingCounter != 3) {
-                if (program == "Synthetics" || program == "synthetics") {
-                    _temperature = _random.Next(MINIMUM_TEMP, SYNTHETICS_MAX + 1);
-                    Console.WriteLine(output);
-                } else if (program == "Silk" || program == "silk") {
-                    _temperature = _random.Next(SYNTHETICS_MAX + 1, SILK_MAX + 1);
-                    Console.WriteLine(output);
-                } else if (program == "Cotton" || program == "cotton") {
-                    _temperature = _random.Next(SILK_MAX + 1, COTTON_MAX + 1);
-                    Console.WriteLine(output);
-                } else {
-                    Console.WriteLine("Invalid program!");
-                }
-                _steamSwitch = 0;
-            } else
-                Console.WriteLine("The machine has been used 3 times and needs cleaning.");
+                if (_ironingCounter != 3) {
+                    if (program == "Synthetics" || program == "synthetics") {
+                        _temperature = _random.Next(MINIMUM_TEMP, SYNTHETICS_MAX + 1);
+                        Console.WriteLine(output);
+                    } else if (program == "Silk" || program == "silk") {
+                        _temperature = _random.Next(SYNTHETICS_MAX + 1, SILK_MAX + 1);
+                        Console.WriteLine(output);
+                    } else if (program == "Cotton" || program == "cotton") {
+                        _temperature = _random.Next(SILK_MAX + 1, COTTON_MAX + 1);
+                        Console.WriteLine(output);
+                    } else {
+                        Console.WriteLine("Invalid program!");
+                    }
+                    _steamSwitch = 0;
+                } else
+                    Console.WriteLine("The machine has been used 3 times and needs cleaning.");
+            }
         }
 
         public virtual void UseSteam() {
-            if (_steamSwitch == 1) {
-                Console.WriteLine("The steam is already on!");
-            } else if (_temperature <= SYNTHETICS_MAX) {
-                Console.WriteLine("The temperature must be at least 120 degrees to use steam!");
+            if (!_isPowerOn) {
+                Console.WriteLine("Turn the machine on first!");
             } else {
-                Console.WriteLine("Ironing with steam.");
-                _steamSwitch = 1;
+                if (_steamSwitch == 1) {
+                    Console.WriteLine("The steam is already on!");
+                } else if (_temperature <= SYNTHETICS_MAX) {
+                    Console.WriteLine("The temperature must be at least 120 degrees to use steam!");
+                } else {
+                    Console.WriteLine("Ironing with steam.");
+                    _steamSwitch = 1;
+                }
             }
         }
 
@@ -100,21 +114,29 @@ namespace HW06 {
         private int _steamCounter;
         
         public override void DoIroning(int temperature) {
-            if (_ironingCounter != 3) {
-                base.DoIroning(temperature);
-            } else
-                Descale();
+            if (!_isPowerOn) {
+                Console.WriteLine("Turn the machine on first!");
+            } else {
+                if (_ironingCounter != 3) {
+                    base.DoIroning(temperature);
+                } else
+                    Descale();
+            }
         }
         
         public override void UseSteam() {
-            base.UseSteam();
-
-            if (_waterIndicator == 1) {
-                Console.WriteLine("Add more water to use steam!");
-            } else if (_steamCounter != 2) {
-                _steamCounter++;
+            if (!_isPowerOn) {
+                Console.WriteLine("Turn the machine on first!");
             } else {
-                _waterIndicator = 1;
+                base.UseSteam();
+
+                if (_waterIndicator == 1) {
+                    Console.WriteLine("Add more water to use steam!");
+                } else if (_steamCounter != 2) {
+                    _steamCounter++;
+                } else {
+                    _waterIndicator = 1;
+                }
             }
         }
     }
@@ -123,28 +145,35 @@ namespace HW06 {
         private const int LINEN_MAX = 230;
         
         public override void DoIroning(int temperature) {
-            _temperature = temperature;
-            _machineType = "Linen machine";
-            
-            if (_temperature > SILK_MAX && _temperature <= LINEN_MAX) {
-                Console.WriteLine(_machineType + " is ironing with Linen program.");
-            }
+            if (!_isPowerOn) {
+                Console.WriteLine("Turn the machine on first!");
+            } else {
+                _temperature = temperature;
+                _machineType = "Linen machine";
 
-            if (_ironingCounter != 3) {
-                base.DoIroning(temperature);
+                if (_ironingCounter != 3) {
+                    if (_temperature > SILK_MAX && _temperature <= LINEN_MAX) {
+                        Console.WriteLine(_machineType + " is ironing with Linen program.");
+                    } else
+                        base.DoIroning(temperature);
+                }
             }
         }
 
         public override void DoIroning(string program) {
-            string output = string.Format(_output, _machineType, _temperature);
+            if (!_isPowerOn) {
+                Console.WriteLine("Turn the machine on first!");
+            } else {
+                string output = string.Format(_output, _machineType, _temperature);
 
-            if (program == "Linen") {
-                _temperature = _random.Next(COTTON_MAX + 1, LINEN_MAX + 1);
-                UseSteam();
-                Console.WriteLine(output);
+                if (program == "Linen") {
+                    _temperature = _random.Next(COTTON_MAX + 1, LINEN_MAX + 1);
+                    UseSteam();
+                    Console.WriteLine(output);
+                }
+
+                base.DoIroning(program);
             }
-            
-            base.DoIroning(program);
         }
     }
 }
