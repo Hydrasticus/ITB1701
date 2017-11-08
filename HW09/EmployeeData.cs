0ed5dd63c9b73ca07e54951f142d6725e5060598
Alo Pullmann <alpull@ttu.ee>
2017-11-09 00:44:41 +0200

    HW09: added some last-minute logic

--- HW09/EmployeeData.cs
+++ HW09/EmployeeData.cs
@@ -11,7 +11,21 @@ namespace HW09 {
             string lastName = nameArray[1];
 
             if (firstName.Contains("-")) {
-                
+                string[] firstNameArray = firstName.Split('-');
+                List<string> firstNames = new List<string>();
+                foreach (string nameInArray in firstNameArray) {
+                    firstNames.Add(nameInArray);
+                }
+                firstName = string.Join(" ", firstNames);
+            }
+
+            if (lastName.Contains("_")) {
+                string[] lastNameArray = lastName.Split('_');
+                List<string> lastNames = new List<string>();
+                foreach (string nameInArray in lastNameArray) {
+                    lastNames.Add(nameInArray);
+                }
+                lastName = string.Join("-", lastNames);
             }
             
             return $"{firstName} {lastName}";
